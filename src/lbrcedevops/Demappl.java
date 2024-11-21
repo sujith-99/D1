9f0a40cb1bd8c8118ac052997c1abc7fa8317d47
sujith-99 <127016122+sujith-99@users.noreply.github.com>
2024-11-21 22:16:07 +0530

    java commit

---
+++ src/lbrcedevops/Demappl.java
@@ -0,0 +1,11 @@
+package lbrcedevops;
+
+public class Demappl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Successfully Completed");
+
+	}
+
+}
